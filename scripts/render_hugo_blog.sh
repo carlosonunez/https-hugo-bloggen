0e761abcbdd1f26dbb24330c773fa763b0420ecd
Carlos Nunez <carlosonunez@noreply.github.com>
2018-10-05 19:16:07 -0500

    Add a function that allows us to spin up a temporary Hugo server for spot-checking things. This might be useful for manually confirming a layout change that's difficult to express as a test.

--- scripts/render_hugo_blog.sh
+++ scripts/render_hugo_blog.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 HUGO_VERSION="${HUGO_VERSION?Please provide a version of Hugo to install.}"
-HUGO_THEME_URL="${HUGO_THEME_URL?Please provide the URL of the Hugo theme to use.}"
+HUGO_THEME="${HUGO_THEME?Please provide a theme to use.}"
 GENERATED_HUGO_DOCKER_IMAGE_NAME="${GENERATED_HUGO_DOCKER_IMAGE_NAME:-blog_carlosnunez_me}"
 LOCAL_PORT_TO_EXPOSE_HUGO_TO="${LOCAL_PORT_TO_EXPOSE_HUGO_TO:-8080}"
 ENVIRONMENT_FILE="${ENVIRONMENT_FILE:-/env}"
 HUGO_CONTAINER_NAME=hugo-session-$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | head -c 8)
 HOST_PWD="${HOST_PWD:-$PWD}"
 HUGO_BASE_URL="${HUGO_BASE_URL:-http://localhost}"
+KEEP_HUGO_SERVER_ALIVE_FOR_TESTING="${KEEP_HUGO_SERVER_ALIVE_FOR_TESTING:-false}"
 NUMBER_OF_TIMES_TO_RETRY_CONNECTING_TO_HUGO=3
-PATH_TO_TEST_POST=""
+POST_NAME=""
 
 display_docker_in_docker_warning() {
   if [ "$HOST_PWD" != "$PWD" ]
@@ -33,9 +34,6 @@ Environment variables:
   LOCAL_PORT_TO_EXPOSE_HUGO_TO        The port to expose Hugo on.
                                       (Currently: $LOCAL_PORT_TO_EXPOSE_HUGO_TO)
 
-  HUGO_THEME_URL                      The URL of the theme to download.
-                                      (Currently: $HUGO_THEME_URL)
-
   ENVIRONMENT_FILE                    An environment file containing Hugo
                                       configuration data.
                                       NOTE: config.toml is automatically-generated
@@ -59,17 +57,6 @@ build_hugo_docker_image() {
     . >/dev/null
 }
 
-rebuild_hugo_theme() {
-  >&2 echo "INFO: Rebuilding Hugo theme."
-  theme_name=$(echo "$HUGO_THEME_URL" | awk -F'/' '{print $NF}' | sed 's/.git//')
-  if [ -d site/themes ]
-  then
-    rm -rf site/themes
-  fi
-  mkdir site/themes
-  git clone -q "$HUGO_THEME_URL" "site/themes/$theme_name" >/dev/null
-}
-
 initialize_hugo_config() {
   >&2 echo "INFO: Initializing Hugo config."
   echo '' > site/config.toml
@@ -77,27 +64,39 @@ initialize_hugo_config() {
 
 start_hugo() {
   >&2 echo "INFO: Starting Hugo."
-  docker run \
-    --detach \
-    --name="${HUGO_CONTAINER_NAME}" \
-    --env-file="${ENVIRONMENT_FILE}" \
-    --env "HUGO_THEME=$(echo $HUGO_THEME_URL | awk -F'/' '{print $NF}' | sed 's/.git$//')" \
-    --volume "$HOST_PWD/site:/site" \
-    --publish 8080:8080 \
-    ${GENERATED_HUGO_DOCKER_IMAGE_NAME} hugo server --baseURL ${HUGO_BASE_URL} \
-      --bind 0.0.0.0 \
-      -p 8080 >/dev/null
+  if [ "$KEEP_HUGO_SERVER_ALIVE_FOR_TESTING" == "false" ]
+  then
+    docker run \
+      --detach \
+      --name="${HUGO_CONTAINER_NAME}" \
+      --env-file="${ENVIRONMENT_FILE}" \
+      --volume "$HOST_PWD/site:/site" \
+      --publish 8080:8080 \
+      ${GENERATED_HUGO_DOCKER_IMAGE_NAME} hugo server --baseURL ${HUGO_BASE_URL} \
+        --bind 0.0.0.0 \
+        -p 8080 >/dev/null
+  else
+    docker run \
+      --name="${HUGO_CONTAINER_NAME}" \
+      --env-file="${ENVIRONMENT_FILE}" \
+      --volume "$HOST_PWD/site:/site" \
+      --publish 8080:8080 \
+      ${GENERATED_HUGO_DOCKER_IMAGE_NAME} hugo server --baseURL ${HUGO_BASE_URL} \
+        --bind 0.0.0.0 \
+        -p 8080
+  fi
 }
 
 test_hugo() {
   for attempt in $(seq 1 $NUMBER_OF_TIMES_TO_RETRY_CONNECTING_TO_HUGO)
   do
-    >&2 echo "INFO: Testing Hugo connectivity (attempt $attempt/$NUMBER_OF_TIMES_TO_RETRY_CONNECTING_TO_HUGO)"
     slug_to_test='about'
-    if [ ! -z "$PATH_TO_TEST_POST" ]
+    if [ ! -z "$POST_NAME" ]
     then
-      slug_to_test=$(echo "$PATH_TO_TEST_POST" | sed 's#/content/##')
+      slug_to_test="post/$POST_NAME"
     fi
+    >&2 echo "INFO: Testing Hugo connectivity, slug '$slug_to_test' \
+(attempt $attempt/$NUMBER_OF_TIMES_TO_RETRY_CONNECTING_TO_HUGO)"
     return_code=$(curl -o /dev/null -sLvvvvw '%{http_code}' "http://localhost:8080/$slug_to_test")
     case "$return_code" in
       2*)
@@ -115,13 +114,14 @@ test_hugo() {
 }
 
 make_a_test_post_if_applicable() {
-  if [ ! -z "$PATH_TO_TEST_POST" ]
+  if [ ! -z "$POST_NAME" ]
   then
-    if [ ! -d "sites/$(dirname $PATH_TO_TEST_POST)" ]
+    if [ ! -d 'site/content/post' ]
     then
-      mkdir -p "sites/$(dirname $PATH_TO_TEST_POST)"
+      mkdir -p 'site/content/post'
     fi
-    cat >"sites/$PATH_TO_TEST_POST" <<TEST_POST
+    cat >"site/content/post/${POST_NAME}.md" <<TEST_POST
+---
 Lorem ipsum dolor sit amet, consectetur adipiscing elit, \
 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. \
 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut \
@@ -134,7 +134,7 @@ TEST_POST
 }
 
 remove_test_post_if_applicable() {
-  rm -f "sites/$PATH_TO_TEST_POST"
+  rm -f "site/content/post/${POST_NAME}.md"
 }
 
 stop_hugo() {
@@ -150,15 +150,22 @@ then
   usage
   exit 0
 else
-  PATH_TO_TEST_POST="$1"
+  POST_NAME="$1"
 fi
 
 initialize_hugo_config &&
-rebuild_hugo_theme &&
-make_a_test_post_if_applicable &&
-build_hugo_docker_image &&
-start_hugo &&
-test_hugo;
-result=$?
-stop_hugo && remove_test_post_if_applicable
-exit $result
+build_hugo_docker_image
+if [ "$KEEP_HUGO_SERVER_ALIVE_FOR_TESTING" == "false" ]
+then
+  make_a_test_post_if_applicable &&
+  start_hugo &&
+  test_hugo;
+  result=$?
+  stop_hugo && remove_test_post_if_applicable
+  exit $result
+else
+  >&2 echo "WARN: Asked to keep a Hugo server up for testing. \
+Remember to kill your Docker containers after you're done: \
+docker rm ${HUGO_CONTAINER_NAME} -f"
+  start_hugo
+fi
