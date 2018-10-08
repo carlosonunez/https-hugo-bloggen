8ab8f4823a2b62b838b2425df445b64072382695
Carlos Nunez <carlosonunez@noreply.github.com>
2018-10-07 23:31:28 -0500

    Add support for remote state backends.

--- include/make/providers/aws.mk
+++ include/make/providers/aws.mk
@@ -1,7 +1,7 @@
 #!/usr/bin/env make
 define PROVIDER_AWS_USAGE
 $@
-Configures the AWS cloud provider.
+Configures environment variables and Terraform configurations for AWS.
 
 Required environment variables:
 
@@ -10,25 +10,50 @@ Required environment variables:
   AWS_ACCESS_KEY_ID       The IAM access key to use for AWS API calls.
 
   AWS_SECRET_ACCESS_KEY   The secret key to use for AWS_ACCESS_KEY_ID
+
+Notes:
+
+- The Terraform backend this provider configures uses the same region as the
+one defined by AWS_REGION. You will need to modify this include if your
+requirements differ.
 endef
 export PROVIDER_AWS_USAGE
 
 ifeq ($(INFRASTRUCTURE_PROVIDER),aws)
 ifndef AWS_ACCESS_KEY_ID
-$(error Please provide an AWS access key to use)
+$(error Please provide an AWS access key to use with the \
+	AWS_ACCESS_KEY_ID environment variable)
 endif
 ifndef AWS_SECRET_ACCESS_KEY
-$(error Please provide an AWS secret key to use)
+$(error Please provide an AWS secret key to use with the \
+	AWS_SECRET_ACCESS_KEY environment variable)
 endif
 ifndef AWS_REGION
-$(error Please define the AWS region to use)
+$(error Please define the AWS region to use with the \
+	AWS_REGION environment variable)
+endif
+ifndef TERRAFORM_S3_BUCKET
+$(error Please define the S3 bucket to use for storing state with the \
+	TERRAFORM_S3_BUCKET environment variable)
+endif
+ifndef TERRAFORM_S3_KEY
+	$(error Please use the TERRAFORM_S3_KEY environment variable to define the \
+		AWS S3 key to store Terraform state inside of))
 endif
 endif
+
+# Terraform backend definitions
+define AWS_TERRAFORM_STATE_BACKEND_CONFIGURATION
+bucket = "$(TERRAFORM_S3_BUCKET)"
+key = "$(TERRAFORM_S3_KEY)"
+region = "$(AWS_REGION)"
+endef
 
 # Terraform exports.
 export TF_VAR_aws_access_key = $(AWS_ACCESS_KEY_ID)
 export TF_VAR_aws_secret_key = $(AWS_SECRET_ACCESS_KEY)
 export TF_VAR_aws_region = $(AWS_REGION)
+
 
 .PHONY: provider_aws_usage
 provider_aws_usage:
--- include/make/terraform.mk
+++ include/make/terraform.mk
@@ -5,7 +5,7 @@ Interface for running Terraform actions.
 
 Targets:
 
-	terraform_usage             Displays this help message.
+  terraform_usage             Displays this help message.
 
   terraform_plan              Calculates and displays a Terraform plan.
 
@@ -35,10 +35,42 @@ Optional Environment Variables:
   INFRASTRUCTURE_DIRECTORY    Directory containing Terraform code.
                               (Default: $(INFRASTRUCTURE_DIRECTORY))
 
+  BACKEND_PROVIDER            The backend provider to use for managing
+                              remote state. Defaults to INFRASTRUCTURE_PROVIDER.
+                              This is useful if you want to use a non-cloud
+                              based backend like Consul. Note that you
+                              will still need to create a Makefile include
+                              at include/make/BACKEND_PROVIDER.mk and define
+                              (BACKEND_PROVIDER)_TERRAFORM_STATE_BACKEND_CONFIGURATION
+                              there.
+                              (Default: $(BACKEND_PROVIDER))
+
+Notes:
+
+- Every provider Make include *must* define a TERRAFORM_$${PROVIDER}_STATE_BACKEND_CONFIGURATION
+  Make variable containing options for that provider's backend.
 endef
 export TERRAFORM_USAGE
 
+ifndef INFRASTRUCTURE_PROVIDER
+$(error Please define an infrastructure provider for Terraform to use)
+endif
+
+INFRASTRUCTURE_PROVIDER_UPCASE := \
+	$(shell echo $(INFRASTRUCTURE_PROVIDER) | tr '[:lower:]' '[:upper:]')
+BACKEND_PROVIDER ?= $(INFRASTRUCTURE_PROVIDER_UPCASE)
+
+ifndef $(BACKEND_PROVIDER)_TERRAFORM_STATE_BACKEND_CONFIGURATION
+$(error This Make include uses remote backends for managing Terraform state \
+	by default. Please define your backend with the \
+	$(BACKEND_PROVIDER)_TERRAFORM_STATE_BACKEND_CONFIGURATION \
+	Make variable in: include/make/$(shell echo $(BACKEND_PROVIDER) | tr '[:upper:]' '[:lower:]').mk)
+endif
+export $(BACKEND_PROVIDER)_TERRAFORM_STATE_BACKEND_CONFIGURATION
+
 INFRASTRUCTURE_DIRECTORY ?= '$(PWD)/infrastructure/$(INFRASTRUCTURE_PROVIDER)'
+BACKEND_TFVARS_LOCATION := $(INFRASTRUCTURE_DIRECTORY)/backend.tfvars
+TERRAFORM_TFVARS_LOCATION := $(INFRASTRUCTURE_DIRECTORY)/terraform.tfvars
 
 .PHONY: \
 	terraform_usage \
@@ -48,10 +80,13 @@ INFRASTRUCTURE_DIRECTORY ?= '$(PWD)/infrastructure/$(INFRASTRUCTURE_PROVIDER)'
 	terraform_run \
 	terraform_generate_variables
 
+terraform_generate_backend:
+	@echo "$$$(BACKEND_PROVIDER)_TERRAFORM_STATE_BACKEND_CONFIGURATION" > $(BACKEND_TFVARS_LOCATION)
+
 terraform_generate_variables:
 	env | \
 		grep -E '^TF_VAR_' | \
-		sed 's/^TF_VAR_\(.*\)=\(.*\)/\1 = "\2"/' > infrastructure/$(INFRASTRUCTURE_PROVIDER)/terraform.tfvars
+		sed 's/^TF_VAR_\(.*\)=\(.*\)/\1 = "\2"/' > $(TERRAFORM_TFVARS_LOCATION)
 
 terraform_usage:
 	@echo "$$TERRAFORM_USAGE"
@@ -62,7 +97,7 @@ terraform_validate:
 terraform_init:
 	if [ ! -f .terraform_is_initialized ]; \
 	then \
-		$(MAKE) terraform_run TERRAFORM_ACTION=init && \
+		$(MAKE) terraform_generate_backend && \
 		$(MAKE) terraform_generate_variables && \
 		touch .terraform_is_initialized; \
 	fi
