bffa1983c88283c858ffd9c678e60172ac1f74d8
Carlos Nunez <carlosonunez@noreply.github.com>
2018-10-13 15:17:32 -0400

    Add our S3 bucket

---
+++ infrastructure/aws/hosting.tf
@@ -0,0 +1,21 @@
+locals {
+  s3_bucket_name = "${random_string.bucket_prefix.result}-${var.s3_bucket_name}"
+}
+
+resource "random_string" "bucket_prefix" {
+  length = "${var.s3_bucket_name_prefix_length}"
+}
+
+data "aws_iam_policy_document" "make_website_world_readable" {
+  statement {
+    sid = "PublicReadGetObject"
+    effect = "Allow"
+    actions = [ "s3:GetObject" ]
+    resources = [ "arn:aws:s3:::${var.s3_bucket_name}/*" ]
+}
+
+resource "aws_s3_bucket" "blog" {
+  bucket = "${local.s3_bucket_name}"
+  acl = "public-read"
+  policy = "${data.aws_iam_policy_document.make_website_world_readable.json}"
+}
--- infrastructure/aws/variables.tf
+++ infrastructure/aws/variables.tf
@@ -39,3 +39,18 @@ They must be within the CIDR declared by vpc_cidr, and they must equal
 the number of AZs requested by 'number_of_azs'
 EOF
 }
+
+variable "s3_bucket_name" {
+  description = <<EOF
+The name to assign to the S3 bucket created for this blog. Ensure that your
+bucket name conforms to AWS S3 guidelines, as this will be validated at
+apply-time, not plan-time.
+See this document for more details: https://docs.aws.amazon.com/AmazonS3/latest/dev/BucketRestrictions.html
+NOTE: A random prefix will be added to the name to avoid bucket colocation.
+EOF
+}
+
+variable "s3_bucket_name_prefix_length" {
+  description = "The length of the prefix to assign to the S3 bucket name."
+  default = 8
+}
