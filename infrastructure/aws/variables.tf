97fbef3c4a3ff2403d685864e27c0a9685aff294
Carlos Nunez <carlosonunez@noreply.github.com>
2018-10-13 14:16:18 -0400

    Add AWS VPC variables

--- infrastructure/aws/variables.tf
+++ infrastructure/aws/variables.tf
@@ -9,3 +9,24 @@ variable "aws_access_key" {
 variable "aws_secret_key" {
   description = "The secret key to use."
 }
+
+variable "number_of_azs" {
+  description = "The number of availability zones to use."
+}
+
+variable "vpc_cidr" {
+  description = "The CIDR to assign to the VPC."
+}
+
+variable "environment_name" {
+  description = "The name of the environment to provision."
+}
+
+variable "private_vpc_subnet_cidrs" {
+  type = "list"
+  description = <<EOF
+CIDRs to use for private subnets created within the VPC.
+They must be within the CIDR declared by vpc_cidr, and they must equal
+the number of AZs requested by 'number_of_azs'
+EOF
+}
