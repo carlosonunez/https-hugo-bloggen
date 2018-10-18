80b2abea85044a1c2e907e0d9f1c0ed56517b1c7
Carlos Nunez <carlosonunez@noreply.github.com>
2018-10-18 09:48:30 -0400

    Try removing dependency on s3_bucket_name var

--- infrastructure/aws/data.tf
+++ infrastructure/aws/data.tf
@@ -1,5 +1,10 @@
 data "aws_availability_zones" "available" {}
+data "aws_route53_zone" "found" {
+  name = "${var.route53_domain_name}."
+  private_zone = false
+}
 locals {
-  s3_bucket_name = "${var.s3_bucket_name}"
-  s3_bucket_origin_id = "${var.s3_bucket_name}"
+  s3_bucket_name = "${replace(var.hugo_base_url), "^(http|https):\/\/(.*)$", "$2"}"
+  s3_bucket_origin_id = "${local.s3_bucket_name}"
+  route53_record_name = "${replace(var.hugo_base_url, "^(http|https):\/\/([a-zA-Z0-9-_])\.${var.route53_domain_name}$","$1")}"
 }
