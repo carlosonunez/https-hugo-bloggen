1b0600ffb54f414c5bd0e74c3f527a3c01106373
Carlos Nunez <carlosonunez@noreply.github.com>
2018-12-31 12:19:22 -0600

    Target health should not be evaluated if the ALIAS record is pointing directly at the S3 bucket.

--- infrastructure/aws/dns.tf
+++ infrastructure/aws/dns.tf
@@ -1,10 +1,10 @@
 resource "aws_route53_record" "blog" {
   zone_id = "${data.aws_route53_zone.found.zone_id}"
   name = "${local.route53_record_name}"
-  type = "CNAME"
+  type = "A"
   alias {
     name = "${var.enable_cloudfront_cdn ? element(concat(aws_cloudfront_distribution.blog.*.domain_name, list("")), 0) : aws_s3_bucket.blog.website_domain}"
     zone_id = "${var.enable_cloudfront_cdn ? element(concat(aws_cloudfront_distribution.blog.*.hosted_zone_id, list("")), 0) : aws_s3_bucket.blog.hosted_zone_id}"
-    evaluate_target_health = true
+    evaluate_target_health = "${var.enable_cloudfront_cdn ? true : false }"
   }
 }
