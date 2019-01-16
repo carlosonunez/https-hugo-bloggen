e775dded51ef7f1017a1ce7e0395ce4aa9b91635
Carlos Nunez <carlosonunez@noreply.github.com>
2019-01-16 17:50:58 -0500

    Allow the current working directory to change if we are running this within nested Docker.

--- include/make/docker.mk
+++ include/make/docker.mk
@@ -1,5 +1,5 @@
 #!/usr/bin/env make
-EXPANDED_DOCKER_COMPOSE_COMMAND := docker-compose -f docker-compose.yml $(shell for file in include/compose/*.yml; do echo "-f $$file"; done)
+EXPANDED_DOCKER_COMPOSE_COMMAND := HOST_PWD=$(PWD) docker-compose -f docker-compose.yml $(shell for file in include/compose/*.yml; do echo "-f $$file"; done)
 DOCKER_COMPOSE_COMMAND := $(EXPANDED_DOCKER_COMPOSE_COMMAND) --log-level CRITICAL
 ifeq ($(SHOW_DOCKER_COMPOSE_LOGS),true)
 DOCKER_COMPOSE_COMMAND := $(EXPANDED_DOCKER_COMPOSE_COMMAND) --log-level INFO
