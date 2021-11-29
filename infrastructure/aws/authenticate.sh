c1cb8a2075a3322dd3a93891ed1b54dfbe5bd1cb
Carlos Nunez <carlosonunez@noreply.github.com>
2021-11-29 14:35:10 -0600

    Remove local depenency on jq and between Compose and platform scripts

--- infrastructure/aws/authenticate.sh
+++ infrastructure/aws/authenticate.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
+AWS_DOCKER_IMAGE="amazon/aws-cli:2.2.9"
+JQ_DOCKER_IMAGE="imega/jq:1.6"
+export ENV_FILE="${1?Please provide a dotenv for the AWS authenticator}"
 while read -r kv
 do
   export "$kv"
-done < <(egrep -Ev '^#' "$1" | xargs -0)
-env | grep AWS
-if ! output=$(docker-compose run --rm -T aws sts assume-role \
+done < <(egrep -Ev '^#' "$ENV_FILE" | xargs -0)
+if ! output=$(docker run --rm --env-file "$ENV_FILE" "$AWS_DOCKER_IMAGE" sts assume-role \
   --role-arn "$AWS_ROLE_ARN" \
   --external-id "$AWS_STS_EXTERNAL_ID" \
   --role-session-name "bloggen-deployment-$(date +%s)" \
@@ -16,7 +18,7 @@ fi
 >&2 echo "DEBUG: got output: $output"
 
 echo "$output" | \
-  jq -r '"AWS_ACCESS_KEY_ID=" + .Credentials.AccessKeyId + " " +
+  docker run --rm -i "$JQ_DOCKER_IMAGE" -r '"AWS_ACCESS_KEY_ID=" + .Credentials.AccessKeyId + " " +
 "AWS_SECRET_ACCESS_KEY=" + .Credentials.SecretAccessKey + " " +
 "AWS_SESSION_TOKEN=" + .Credentials.SessionToken' | \
   tr ' ' "\n"
--- infrastructure/aws/copy.sh
+++ infrastructure/aws/copy.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-docker-compose run --rm aws s3 sync "$1" "s3://$(basename "$HUGO_BASE_URL")"
+AWS_DOCKER_IMAGE="amazon/aws-cli:2.2.9"
+docker run --rm "$AWS_DOCKER_IMAGE" s3 sync "$1" "s3://$(basename "$HUGO_BASE_URL")"
