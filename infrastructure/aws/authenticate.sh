f7053dd5128bcf07a70c3dc233c644ab774e9459
Carlos Nunez <carlosonunez@noreply.github.com>
2021-11-29 12:59:33 -0600

    Debug AWS authenticate

--- infrastructure/aws/authenticate.sh
+++ infrastructure/aws/authenticate.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
-docker-compose run --rm -T aws sts assume-role \
+if ! output=$(docker-compose run --rm -T aws sts assume-role \
   --role-arn "$AWS_ROLE_ARN" \
   --external-id "$AWS_STS_EXTERNAL_ID" \
   --role-session-name "bloggen-deployment-$(date +%s)" \
-  --output json | \
+  --output json)
+then
+  >&2 echo "ERROR: Failed to get creds, output: $output"
+  exit 1
+fi
+
+echo "$output" | \
   jq -r '"AWS_ACCESS_KEY_ID=" + .Credentials.AccessKeyId + " " +
 "AWS_SECRET_ACCESS_KEY=" + .Credentials.SecretAccessKey + " " +
 "AWS_SESSION_TOKEN=" + .Credentials.SessionToken' | \
