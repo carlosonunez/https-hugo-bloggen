e416f991d1f987ab1c6b643de115c56704e0d09f
Carlos Nunez <carlosonunez@noreply.github.com>
2021-11-29 14:18:35 -0600

    pass env var into authenticate scripts

--- infrastructure/aws/authenticate.sh
+++ infrastructure/aws/authenticate.sh
@@ -3,6 +3,7 @@ while read -r kv
 do
   export "$kv"
 done < <(egrep -Ev '^#' "$1" | xargs -0)
+env | grep AWS
 if ! output=$(docker-compose run --rm -T aws sts assume-role \
   --role-arn "$AWS_ROLE_ARN" \
   --external-id "$AWS_STS_EXTERNAL_ID" \
@@ -12,6 +13,7 @@ then
   >&2 echo "ERROR: Failed to get creds, output: $output"
   exit 1
 fi
+>&2 echo "DEBUG: got output: $output"
 
 echo "$output" | \
   jq -r '"AWS_ACCESS_KEY_ID=" + .Credentials.AccessKeyId + " " +
