bbb0e53f0efa1b3005d2c3206d0f7ecc071361b5
Carlos Nunez <carlosonunez@noreply.github.com>
2021-11-29 14:11:47 -0600

    pass env var into authenticate scripts

--- infrastructure/aws/authenticate.sh
+++ infrastructure/aws/authenticate.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+while read -r kv
+do
+  export "$kv"
+done < <(egrep -Ev '^#' "$1" | xargs -0)
 if ! output=$(docker-compose run --rm -T aws sts assume-role \
   --role-arn "$AWS_ROLE_ARN" \
   --external-id "$AWS_STS_EXTERNAL_ID" \
