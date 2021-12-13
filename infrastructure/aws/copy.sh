926867d4f862db0eb925bcec0a0dfbff4724d231
Carlos Nunez <carlosonunez@noreply.github.com>
2021-12-12 23:30:40 -0600

    [aws] ensure authentication data received on copy

--- infrastructure/aws/copy.sh
+++ infrastructure/aws/copy.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 AWS_DOCKER_IMAGE="amazon/aws-cli:2.2.9"
-docker run --rm "$AWS_DOCKER_IMAGE" s3 sync "$1" "s3://$(basename "$HUGO_BASE_URL")"
+set -x
+docker run --rm \
+  -v "$1:/work" \
+  -w /work \
+  -e AWS_ACCESS_KEY_ID \
+  -e AWS_SECRET_ACCESS_KEY \
+  -e AWS_SESSION_TOKEN \
+  -e AWS_REGION \
+  "$AWS_DOCKER_IMAGE" s3 sync "." "s3://$(basename "$HUGO_BASE_URL")"
