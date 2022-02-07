90e11b503443ff6f91c96e22d989dcd873fe1b70
Carlos Nunez <carlosonunez@noreply.github.com>
2022-02-06 22:33:54 -0600

    fix: allow regions other than us-east-1 except for ACM

--- infrastructure/aws/certificates.tf
+++ infrastructure/aws/certificates.tf
@@ -1,4 +1,5 @@
 resource "aws_acm_certificate" "aws_managed_https_certificate" {
+  provider = "aws.acm-required-in-us-east-1-for-cloudfront"
   count   = "${var.enable_cloudfront_cdn == true ? 1 : 0}"
   tags = "${local.default_tags}"
   domain_name = "${local.blog_fqdn_requested}"
@@ -19,6 +20,7 @@ resource "aws_route53_record" "aws_managed_https_certificate_validation_record" 
 
 resource "aws_acm_certificate_validation" "aws_managed_https_certificate" {
   count   = "${var.enable_cloudfront_cdn == true ? 1 : 0}"
+  provider = "aws.acm-required-in-us-east-1-for-cloudfront"
   certificate_arn         = "${aws_acm_certificate.aws_managed_https_certificate[0].arn}"
   validation_record_fqdns = ["${aws_route53_record.aws_managed_https_certificate_validation_record[0].fqdn}"]
   timeouts {
